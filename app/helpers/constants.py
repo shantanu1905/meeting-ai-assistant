c94496e2827a34afb2d44c8a382af6a3a53183d5
shantanu1905 <nimkarshantanu07@gmail.com>
2025-05-11 22:11:00 +0530

    added constats.py

---
+++ app/helpers/constants.py
@@ -0,0 +1,4 @@
+
+
+DATAPREP_URL = "http://localhost:6007/v1/dataprep/ingest"
+UPLOAD_DIR = "uploaded_files" 
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, FastAPI
-from app.routers import auth , meeting , user_files
+from app.routers import auth , meeting , user_metadata
 from app.local_database.database import Base , engine
 
 app = FastAPI()
@@ -8,8 +8,11 @@ app = FastAPI()
 Base.metadata.create_all(engine)
 app.include_router(auth.router)
 app.include_router(meeting.router)
-app.include_router(user_files.router)
+app.include_router(user_metadata.router)
 
+
+DATAPREP_URL = "http://localhost:6007/v1/dataprep/ingest"
+UPLOADS_DIR = "uploaded_files"  # or your file save directory
 
 
 
