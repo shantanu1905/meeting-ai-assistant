fd3bfbd37d24c523b84adf505ea53330aa8bba32
shantanu1905 <nimkarshantanu07@gmail.com>
2025-05-14 00:24:33 +0530

    added generative ai routes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, FastAPI
-from app.routers import auth , meeting , user_metadata
+from app.routers import auth , meeting , user_metadata , generative_ai
 from app.local_database.database import Base , engine
 
 app = FastAPI()
@@ -9,6 +9,8 @@ Base.metadata.create_all(engine)
 app.include_router(auth.router)
 app.include_router(meeting.router)
 app.include_router(user_metadata.router)
+app.include_router(generative_ai.router)
+
 
 
 DATAPREP_URL = "http://localhost:6007/v1/dataprep/ingest"
