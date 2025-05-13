0f056f43932d95544c6401f51c5cfe3ba481a446
shantanu1905 <nimkarshantanu07@gmail.com>
2025-05-14 00:24:59 +0530

    added urls

--- app/helpers/constants.py
+++ app/helpers/constants.py
@@ -2,3 +2,8 @@
 
 DATAPREP_URL = "http://localhost:6007/v1/dataprep/ingest"
 UPLOAD_DIR = "uploaded_files" 
+RETRIVER_URL = "http://localhost:6008/v1/retrieval"
+EMBEDDING_URL="http://localhost:8090/embed"
+OLLAMA_URL = "http://localhost:11434/api/generate"
+import requests
+import json
