d4f48d91ca5eb10c2cf6f2c36205b7870e524a59
shantanu1905 <nimkarshantanu07@gmail.com>
2025-05-22 22:40:45 +0530

    removed spaces

--- app/helpers/constants.py
+++ app/helpers/constants.py
@@ -5,5 +5,5 @@ UPLOAD_DIR = "uploaded_files"
 RETRIVER_URL = "http://localhost:6008/v1/retrieval"
 EMBEDDING_URL="http://localhost:8090/embed"
 OLLAMA_URL = "http://localhost:11434/api/generate"
-import requests
-import json
+
+
